33cde2bf93fd42a3fe8aa1ec0a566817187c2546
Valdecir Junior <valdecir.silva9@fatec.sp.gov.br>
2021-02-17 18:53:43 -0300

    Correção link da home

--- pages/sobre.js
+++ pages/sobre.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 function Sobre() {
     return <div>
         <h1>Sobre</h1>
-        <Link href="/sobre">
+        <Link href="/home">
             <a>Acessar página Home</a>
         </Link>
     </div>
